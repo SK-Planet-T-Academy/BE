ee4a835990c28e87bd9b8aaec08133b36de400e0
Mac_BaekMinJun <baekminjun92@gmail.com>
2025-03-28 02:21:36 +0900

    [feat] WebConfig와 properties 설정

---
+++ src/main/java/com/asac7_hackathon/hackathon/domain/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.asac7_hackathon.hackathon.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+        .allowedOrigins("http://localhost:3000", "http://10.10.140.210:3000") // 필요시 프론트 주소 추가
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowCredentials(true);
+  }
+}
